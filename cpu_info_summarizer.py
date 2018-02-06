0341c57b1e513f5c23a8500cde9d7628c7d7f1df
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2018-02-06 21:06:38 +0900

    fix graph style

--- cpu_info_summarizer.py
+++ cpu_info_summarizer.py
@@ -1,7 +1,7 @@
 import csv
 import argparse
-import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="profile")
 parser.add_argument("--directory", "-d", type=str, default="None", help="output file directory")
@@ -23,10 +23,10 @@ for calculator_name in calculator_name_list:
         data_dictionary[calculator_name] = []
         for index, line in enumerate(csv_reader):
             data_dictionary[calculator_name].append(line)
-        data_dictionary[calculator_name] = data_dictionary[calculator_name][-30:]
+        data_dictionary[calculator_name] = data_dictionary[calculator_name][-50:]
 
 # plot data
-fig = plt.figure()
+fig = plt.figure(figsize=(9, 5))
 ax_cpu = fig.add_subplot(2, 1, 1)
 ax_memory = fig.add_subplot(2, 1, 2)
 for key in data_dictionary.keys():
@@ -39,28 +39,32 @@ for key in data_dictionary.keys():
         memory_percent_list.append(float(line[2]))
 
     # cpu usage
-    ax_cpu.plot(time_list, cpu_percent_list, "-o", label=key)
+    ax_cpu.plot(time_list, cpu_percent_list, "-o", ms=5, label=key, alpha=0.75)
 
     # memory usage
-    ax_memory.plot(time_list, memory_percent_list, "-d", label=key)
+    ax_memory.plot(time_list, memory_percent_list, "-d", ms=5, label=key, alpha=0.75)
 
 now = datetime.now()
 
 ax_cpu.set_xlabel("time")
 ax_cpu.set_ylabel("cpu[%]")
-ax_cpu.set_title("cpu usage[%], " + now.strftime("%Y-%d-%m %H:%M"))
+ax_cpu.set_title("cpu usage[%]")
 ax_cpu.grid()
-ax_cpu.legend()
+ax_cpu.legend(loc="right")
 ax_cpu.set_ylim(0, 100)
+diff = max(time_list) - min(time_list)
+ax_cpu.set_xlim(xmin=min(time_list), xmax=max(time_list) + diff * 0.2)
 
 ax_memory.grid()
-ax_memory.legend()
+ax_memory.legend(loc="right")
 ax_memory.set_xlabel("time")
 ax_memory.set_ylabel("memory[%]")
-ax_memory.set_title("memory usage[%]" + now.strftime("%Y-%d-%m %H:%M"))
+ax_memory.set_title("memory usage[%]")
 ax_memory.set_ylim(0, 100)
+diff = max(time_list) - min(time_list)
+ax_memory.set_xlim(xmin=min(time_list), xmax=max(time_list) + diff * 0.2)
 
 fig.autofmt_xdate()
 fig.tight_layout()
-
-plt.show()
+fig.savefig(args.directory + "latest.png", format="png", dpi=150)
+# plt.show()
