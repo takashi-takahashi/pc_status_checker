666bd7fbb8fdcfa2c6bbe10912996e3cf242fabd
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2018-02-06 19:43:25 +0900

    modify the way to specify output directory

--- each_cpu_info_writer.py
+++ each_cpu_info_writer.py
@@ -5,18 +5,16 @@ from datetime import datetime
 
 parser = argparse.ArgumentParser(description="profile")
 parser.add_argument("--name", "-n", type=str, default="None", help="pc_name")
+parser.add_argument("--directory", "-d", type=str, default="None", help="output file directory")
 args = parser.parse_args()
 print(args.name)
 
-# this should be appropriately modified
-directory_name = ""
-
-with open(directory_name + "{0}.csv".format(args.name), "a") as f:
+with open(args.directory + "{0}.csv".format(args.name), "a") as f:
     csv_writer = csv.writer(f)
     now_string = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     line = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             psutil.cpu_percent(interval=0.5),
             psutil.virtual_memory().percent
-    ]
+            ]
     csv_writer.writerow(line)
     print(now_string, psutil.cpu_percent(interval=0.5), psutil.virtual_memory().percent)
