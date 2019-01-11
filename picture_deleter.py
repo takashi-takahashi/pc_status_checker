688e9cedd1f638c9f07b72a74fde6621e8fe1edb
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2019-01-11 16:31:23 +0900

    fix parameter

--- picture_deleter.py
+++ picture_deleter.py
@@ -2,6 +2,7 @@ import time
 import requests
 import calendar
 from datetime import datetime, timedelta
+from tqdm import tqdm
 
 access_token = ""
 channel_id = ""
@@ -10,7 +11,7 @@ files_list_url = 'https://slack.com/api/files.list'
 delete_url = 'https://slack.com/api/files.delete'
 ts_from = str(calendar.timegm((datetime.now() + timedelta(days=-365)).utctimetuple()))
 ts_to = str(calendar.timegm((datetime.now() + timedelta(days=-1)).utctimetuple()))
-count = 20
+count = 100
 data_list = {'token': access_token, 'channels': channel_id, "ts_to": ts_to, 'ts_from': ts_from, 'count': count}
 
 counter = 0
@@ -25,17 +26,17 @@ while True:
         break
 
     else:
-        for file_index, f in enumerate(files):
-            if file_index > 0:
-                pass
-            file_id = f['id']
-            file_name = f['name']
-            if file_name == "latest.png":
-                data_delete = {'token': access_token, 'file': file_id}
+        print("file length / count  : {0} / {1}".format(len(files), count))
+        for file_index, f in tqdm(enumerate(files), total=len(files)):
+            if f['name'] == "latest.png":
+                data_delete = {'token': access_token, 'file': f['id']}
                 delete_request_response = requests.post(url=delete_url, data=data_delete)
                 if not delete_request_response.json()['ok']:
                     print("delete request failed")
                     print(delete_request_response.json())
+                    print(f['name'])
                     print()
-    print("sleeping...")
-    time.sleep(5)
+                time.sleep(1)
+    print("sleeping...", datetime.now())
+    time.sleep(60)
+_
