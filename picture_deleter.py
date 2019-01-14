9878035ccc319bf1bb465d2f09de59f36694c906
Takashi Takahashi <takahashi.t.cc@m.titech.ac.jp>
2019-01-14 17:00:25 +0900

    fix parameters

--- picture_deleter.py
+++ picture_deleter.py
@@ -11,32 +11,38 @@ files_list_url = 'https://slack.com/api/files.list'
 delete_url = 'https://slack.com/api/files.delete'
 ts_from = str(calendar.timegm((datetime.now() + timedelta(days=-365)).utctimetuple()))
 ts_to = str(calendar.timegm((datetime.now() + timedelta(days=-1)).utctimetuple()))
-count = 100
-data_list = {'token': access_token, 'channels': channel_id, "ts_to": ts_to, 'ts_from': ts_from, 'count': count}
+max_count = 1000
+data_list = {'token': access_token, 'channels': channel_id, "ts_to": ts_to, 'ts_from': ts_from, 'count': max_count}
 
 counter = 0
 while True:
-    print("counter", counter)
-    counter += 1
+    try:
+        print("counter", counter)
 
-    file_list_response = requests.post(url=files_list_url, data=data_list)
-    files = file_list_response.json()['files']
-    if len(files) == 0:
-        print("nothing")
-        break
+        file_list_response = requests.post(url=files_list_url, data=data_list)
+        files = file_list_response.json()['files']
+        # if len(files) == 0:
+        #     print("nothing")
+        if len(files) < 50:
+            print("not enough: ", len(files))
+            time.sleep(20)
 
-    else:
-        print("file length / count  : {0} / {1}".format(len(files), count))
-        for file_index, f in tqdm(enumerate(files), total=len(files)):
-            if f['name'] == "latest.png":
-                data_delete = {'token': access_token, 'file': f['id']}
-                delete_request_response = requests.post(url=delete_url, data=data_delete)
-                if not delete_request_response.json()['ok']:
-                    print("delete request failed")
-                    print(delete_request_response.json())
-                    print(f['name'])
-                    print()
-                time.sleep(1)
-    print("sleeping...", datetime.now())
-    time.sleep(60)
-_
+        else:
+            print("file length / count  : {0} / {1}".format(len(files), max_count))
+            for file_index, f in tqdm(enumerate(files), total=len(files)):
+                if f['name'] == "latest.png":
+                    data_delete = {'token': access_token, 'file': f['id']}
+                    delete_request_response = requests.post(url=delete_url, data=data_delete)
+                    if not delete_request_response.json()['ok']:
+                        print("delete request failed")
+                        print(delete_request_response.json())
+                        print(f['name'])
+                        print()
+                    time.sleep(0.8)
+                if file_index > 300:
+                    break
+            counter += 1
+            # print("sleeping...", datetime.now())
+            # time.sleep(150)
+    except Exception as e:
+        print(e)
